fb09045656935882ed0c56536f79134590e7bfb3
Yahya <yahyahussein670@gmail.com>
2022-06-06 20:13:07 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+var currentUser = 'Awesome!!!!!'
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}`
+
+const shortGreeting = `Welcome, ${currentUser[0]} + !`;
+
+console.log(shortGreeting)
